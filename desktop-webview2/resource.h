2c5464ec3c847a1ed0d757ff4a53013e2f22c7b5
Weather-Mister <weatherisgood06@gmail.com>
2026-09-21 11:53:39 +0800

    Add lightweight WebView2 calculator: desktop-webview2/resource.h

---
+++ desktop-webview2/resource.h
@@ -0,0 +1,6 @@
+#pragma once
+#define IDR_INDEX_HTML       101
+#define IDR_CALCULATOR_JS    102
+#define IDR_CALCULATOR_CSS   103
+#define IDR_STANDALONE_CSS   104
+#define IDR_STANDALONE_JS    105
