95c68cb3bbfb4670483141ad721837f093189727
Weather-Mister <weatherisgood06@gmail.com>
2026-09-21 11:45:13 +0800

    Allow calculator shell to fit content

--- desktop-calculator/preload.js
+++ desktop-calculator/preload.js
@@ -3,6 +3,7 @@ const { contextBridge, ipcRenderer } = require('electron');
 contextBridge.exposeInMainWorld('desktopAPI', {
   hideWindow: () => ipcRenderer.send('hide-window'),
   quitApp: () => ipcRenderer.send('quit-app'),
+  resizeToFit: (size) => ipcRenderer.send('resize-to-fit', size),
   onFocusExpression: (callback) => {
     ipcRenderer.on('focus-expression', () => callback());
   }
